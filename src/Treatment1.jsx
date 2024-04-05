22e183bf692e49e5feaf62e491e049f2eff1d92d
StrBoll <phillipboll107@gmail.com>
2024-04-05 16:54:33 -0400

    Brain map page

--- src/Treatment1.jsx
+++ src/Treatment1.jsx
@@ -6,6 +6,8 @@ import Bottom from './bottom';
 import containerImage from './images/BrainswayExplanation.png'; // Import your container image
 import H1image from './images/H1.png';
 import YouTube from 'react-youtube';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
 const Treatment1 = () => {
   const videoId = 'mjA6oQdLr9U';
@@ -28,7 +30,7 @@ const Treatment1 = () => {
     },
     container: {
       width: "100%",
-      height: '1800px',
+      height: '1900px',
       overflow: 'hidden', // Ensure the image doesn't overflow the container
       position: 'relative', // Position the container relative to its parent
     },
@@ -61,7 +63,38 @@ const Treatment1 = () => {
       cursor: 'pointer',
       fontSize: '16px',
     },
-    
+    video: {
+      width: '100%',
+      height: 'auto',
+      position: 'absolute',
+      transform: 'translate(-21%, -125%)',
+    },
+    whiteComponent: {
+      width: '20%',
+      height: 'auto',
+      backgroundColor: 'white',
+      position: 'absolute',
+      left: 'calc(50% + 280px)',
+      top: '50%',
+      transform: 'translate(-50%, 315%)',
+      borderRadius: '7px',
+      borderTopLeftRadius: '49px', 
+      boxShadow: '0px 0px 9px rgba(0, 0, 0, 0.1)',
+      padding: '20px', 
+      textAlign: 'left',
+      fontSize: '18px', 
+      lineHeight: '1.5', 
+      color: '#343a40', 
+      fontFamily: 'Lato, Helvetica, Arial, sans-serif', 
+      textIndent: '0.5em', // Adjust the indentation as needed
+    },
+    firstLine: {
+      paddingLeft: '15%',
+      color: '#4C94C8', // Dark blue color
+      fontFamily: 'Helvetica, Arial, sans-serif', // Apply the Helvetica font
+      fontSize: '18px', // 18 pixels font size
+      fontWeight: 'bold',
+    },
   };
 
   return (
@@ -71,20 +104,31 @@ const Treatment1 = () => {
         <img src={containerImage} alt="Container" style={stylePicture.image} />
         <img src={H1image} alt="H1Image" style={stylePicture.H1} />
       </div>
-     
-    <iframe
-        width="560"
-        height="315"
-        src={`https://www.youtube.com/embed/mjA6oQdLr9U`}
-        frameborder="0"
-        
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-      <Bottom></Bottom>
+      <div style={stylePicture.video}>
+        <h2 style={{left: '0', color: 'Black', fontSize: '225%', marginBottom: '4%', marginRight: '17%', }}>Real Testimonial</h2>
+        <iframe                  
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
       </div>
-  
+      <div style={stylePicture.whiteComponent}>
+        <span style={stylePicture.firstLine}>Facts about TMS</span><br/><br/>
+        <hr style={{ borderTop: '1px solid #DCDCDC', marginBottom: '20px' }} />
+        <FontAwesomeIcon icon={faCircleCheck} style={{ marginRight: '5px', color: '#8FCBFF' }} /> FDA-cleared treatments<br/><br/>
+        <FontAwesomeIcon icon={faCircleCheck} style={{ marginRight: '5px', color: '#8FCBFF' }} /> Effective for medication-resistant patients<br/><br/>
+        <FontAwesomeIcon icon={faCircleCheck} style={{ marginRight: '5px', color: '#8FCBFF' }} /> Short 20-minute sessions<br/><br/>
+        <FontAwesomeIcon icon={faCircleCheck} style={{ marginRight: '5px', color: '#8FCBFF' }} /> Noninvasive<br/><br/>
+        <FontAwesomeIcon icon={faCircleCheck} style={{ marginRight: '5px', color: '#8FCBFF' }} /> No systemic side effects<br/><br/>
+        <FontAwesomeIcon icon={faCircleCheck} style={{ marginRight: '5px', color: '#8FCBFF' }} /> Wide variety of insurance accepted, call to see if you're covered
+      </div>
+      <Bottom />
+    </div>
   );
+  
 };
 
 export default Treatment1;
