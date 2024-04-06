053abebe44a0f6158796ccd5b8f692428f54736c
Justin <roblujm@gmail.com>
2024-04-06 18:15:27 -0400

    trat

--- src/BigBlue.jsx
+++ src/BigBlue.jsx
@@ -1,10 +1,29 @@
-// BlueComponent.js
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import bigBlueImage from './images/bigblue.png'; // Import the bigBlue image
 import overlayImage from './images/Brainsway.webp'; // Import the overlay image
 import explanation from './images/BrainswayExplanation.png';
+
 const BlueComponent = () => {
+  const [isOverlayVisible, setIsOverlayVisible] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Check the viewport width and update the state accordingly
+      setIsOverlayVisible(window.innerWidth > 1200); // Adjust the threshold as needed
+    };
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Call handleResize initially
+    handleResize();
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div
       style={{
@@ -14,23 +33,44 @@ const BlueComponent = () => {
         width: '100%', // Full width
         top: 0, // Positioned at the top of the screen
         left: 0, // Aligned to the left
-        zIndex: 0 // Behind other content
+        zIndex: 0, // Behind other content
       }}
     >
-      <img src={bigBlueImage} alt="bigBlue" style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '0 0 100px 100px' }} />
-      <div
+      <img
+        src={bigBlueImage}
+        alt="bigBlue"
         style={{
-          position: 'absolute', // Positioning the overlay absolutely
-          top: '5%', // Align the overlay to the middle vertically
-          left: '35%', // Align the overlay to the left
-          width: '70%', // Full width
-          height: 'auto', // Half of the height
-          zIndex: 1, // Above the background image
-          borderRadius: '0 0 100px 100px', // Rounding off bottom corners by 100 pixels
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          borderRadius: '0 0 100px 100px',
+          zIndex: 0, // Ensure the bigBlueImage stays behind the overlay image
         }}
-      >
-        <img src={overlayImage} alt="Overlay Image" style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '0 0 100px 100px' }} />
-      </div>
+      />
+      {isOverlayVisible && (
+        <div
+          style={{
+            position: 'absolute', // Positioning the overlay absolutely
+            top: '15%', // Align the overlay to the middle vertically
+            left: '35%', // Align the overlay to the left
+            width: '70%', // Full width
+            height: 'auto', // Half of the height
+            zIndex: 1, // Above the background image
+            borderRadius: '0 0 100px 100px', // Rounding off bottom corners by 100 pixels
+          }}
+        >
+          <img
+            src={overlayImage}
+            alt="Overlay Image"
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              borderRadius: '0 0 100px 100px',
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 };
--- src/Treatment1.jsx
+++ src/Treatment1.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import BlueComponent from './BigBlue';
-import SecondSection from './SecondSectionTreatment';
-import './Treatments.css';
 import Bottom from './bottom';
 import containerImage from './images/BrainswayExplanation.png'; // Import your container image
 import H1image from './images/H1.png';
 import YouTube from 'react-youtube';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { ZIndex } from '@tsparticles/engine';
 
 const Treatment1 = () => {
   const videoId = 'mjA6oQdLr9U';
@@ -17,68 +16,27 @@ const Treatment1 = () => {
       origin: window.location.origin, // Set the origin to your website
     },
   };
-  const stylePicture = {
-    title: {
-      fontSize: '60px',
-      color: 'white',
-      textAlign: 'left',
-      paddingTop: '30%',
-      paddingRight: '50%',
-      fontFamily: 'Ubuntu, sans-serif', // Apply the Ubuntu font
-      fontWeight: 700, // Ensure bold font weight
-      fontStyle: 'normal', // Ensure normal font style
-    },
+  const stylePicture1 = {
     container: {
       width: "100%",
-      height: '1900px',
-      overflow: 'hidden', // Ensure the image doesn't overflow the container
-      position: 'relative', // Position the container relative to its parent
+      maxWidth: "700px", // Adjust maximum width as needed
+      zIndex: 2,
     },
+    
     image: {
-      position: 'absolute', // Position the image absolutely within the container
-      top: '50%', // Position the image 50% from the top of the container
-      left: '50%', // Position the image 50% from the left of the container
-      transform: 'translate(-105%, -130%) scale(0.8)', // Center the image horizontally and vertically, and scale it to 50%
-      maxWidth: '100%', // Ensure the image doesn't exceed the container width
-      maxHeight: '100%', // Ensure the image doesn't exceed the container height
-    },
-    H1: {
-      position: 'absolute', // Position the image absolutely within the container
-      top: '50%', // Position the image 50% from the top of the container
-      left: '50%', // Position the image 50% from the left of the container
-      transform: 'translate(-56%, -42%) scale(0.8)', // Center the image horizontally and vertically, and scale it to 50%
-      maxWidth: '100%', // Ensure the image doesn't exceed the container width
-      maxHeight: '100%', // Ensure the image doesn't exceed the container height
-    },
-    button: {
-      position: 'absolute',
-      bottom: '20px', // Adjust as needed
-      left: '50%', // Adjust as needed
-      transform: 'translateX(-50%)', // Center the button horizontally
-      padding: '10px 20px',
-      backgroundColor: '#007bff',
-      color: 'white',
-      border: 'none',
-      borderRadius: '20px',
-      cursor: 'pointer',
-      fontSize: '16px',
+      width: '100%', // Make the image responsive to its container width
+      height: 'auto', // Maintain aspect ratio
     },
     video: {
       width: '100%',
-      height: 'auto',
-      position: 'absolute',
-      transform: 'translate(-21%, -125%)',
+      marginTop: '2rem', // Adjust spacing as needed
     },
     whiteComponent: {
-      width: '20%',
-      height: 'auto',
+      width: '90%', // Adjust as needed
+      maxWidth: '600px', // Set maximum width to prevent stretching on larger screens
+      margin: '0 auto', // Center horizontally
       backgroundColor: 'white',
-      position: 'absolute',
-      left: 'calc(50% + 280px)',
-      top: '50%',
-      transform: 'translate(-50%, 315%)',
       borderRadius: '7px',
-      borderTopLeftRadius: '49px', 
       boxShadow: '0px 0px 9px rgba(0, 0, 0, 0.1)',
       padding: '20px', 
       textAlign: 'left',
@@ -89,7 +47,43 @@ const Treatment1 = () => {
       textIndent: '0.5em', // Adjust the indentation as needed
     },
     firstLine: {
-      paddingLeft: '15%',
+      color: '#4C94C8', // Dark blue color
+      fontFamily: 'Helvetica, Arial, sans-serif', // Apply the Helvetica font
+      fontSize: '18px', // 18 pixels font size
+      fontWeight: 'bold',
+    },
+  };
+  const stylePicture = {
+    container: {
+      width: "95%",
+      maxWidth: "1200px", // Adjust maximum width as needed
+      margin: "0 auto", // Center horizontally
+    },
+    
+    image: {
+      width: '100%', // Make the image responsive to its container width
+      height: 'auto', // Maintain aspect ratio
+    },
+    video: {
+      width: '100%',
+      marginTop: '2rem', // Adjust spacing as needed
+    },
+    whiteComponent: {
+      width: '90%', // Adjust as needed
+      maxWidth: '600px', // Set maximum width to prevent stretching on larger screens
+      margin: '0 auto', // Center horizontally
+      backgroundColor: 'white',
+      borderRadius: '7px',
+      boxShadow: '0px 0px 9px rgba(0, 0, 0, 0.1)',
+      padding: '20px', 
+      textAlign: 'left',
+      fontSize: '18px', 
+      lineHeight: '1.5', 
+      color: '#343a40', 
+      fontFamily: 'Lato, Helvetica, Arial, sans-serif', 
+      textIndent: '0.5em', // Adjust the indentation as needed
+    },
+    firstLine: {
       color: '#4C94C8', // Dark blue color
       fontFamily: 'Helvetica, Arial, sans-serif', // Apply the Helvetica font
       fontSize: '18px', // 18 pixels font size
@@ -99,21 +93,17 @@ const Treatment1 = () => {
 
   return (
     <div className="special-background">
-      <BlueComponent />
+      <BlueComponent style={{ zIndex: 1 }} />
+      <div style={stylePicture1.container}>
+      <img src={containerImage} alt="Container" style={stylePicture.image} />
+      </div>
       <div style={stylePicture.container}>
-        <img src={containerImage} alt="Container" style={stylePicture.image} />
-        <img src={H1image} alt="H1Image" style={stylePicture.H1} />
+       
+        <img src={H1image} alt="H1Image" style={stylePicture.image} />
       </div>
       <div style={stylePicture.video}>
-        <h2 style={{left: '0', color: 'Black', fontSize: '225%', marginBottom: '4%', marginRight: '17%', }}>Real Testimonial</h2>
-        <iframe                  
-          width="560"
-          height="315"
-          src={`https://www.youtube.com/embed/${videoId}`}
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+        <h2 style={{ color: 'Black', fontSize: '225%', marginBottom: '2%', }}>Real Testimonial</h2>
+        <YouTube videoId={videoId} opts={opts} />
       </div>
       <div style={stylePicture.whiteComponent}>
         <span style={stylePicture.firstLine}>Our Treatment:</span><br/><br/>
@@ -128,7 +118,6 @@ const Treatment1 = () => {
       <Bottom />
     </div>
   );
-  
 };
 
 export default Treatment1;
